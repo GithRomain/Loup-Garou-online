c329eebbe615f8d531e1cf50435a5ddb5748dd1a
Romain Pasquier <romain.pasquier@efrei.net>
2022-08-10 16:06:42 +0200

    push pour front

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,8 +1,12 @@
 <template>
+  <button v-on:click="test">PUSH</button>
+  <tbody v-bind:class="[{white :lightMode === 'white'}]">
   <!-- <div></div> -->
   <Header></Header>
   <!-- <router-view/> -->
+  </tbody>
 </template>
+
 
 <script>
 import Header from "@/components/RouterView";
@@ -12,10 +16,50 @@ export default {
    components: {Header},
   data() {
     return {
+      lightMode: 'white'
     };
   },
+  methods:{
+    test(){
+      var html = document.getElementsByTagName('body');
+      html.classList.add('white');
+    }
+  }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Fira+Sans");
+
+html, body{
+
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Fira Sans", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+}
+
+.white {
+  background-color: var(--lightBackground);
+}
+
+:root
+  {
+  --lightTheme: #7549cc;
+  --lightText: 	#724f36;
+  --lightBrown: #d4b9f0;
+  --lightBordeaux: #9769d2;
+  --lightBackground: #fdf4c0;
+
+  --darkTheme: #772330;
+  --darkText: #d8bc94;
+  --darkTextBold: #b08165;
+  --darkBordeaux: #4d2129;
+  --darkBackground: #382129;
+  }
 </style>
--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -1,6 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "./components/HomePage";
-import UserRegister from "./components/UserRegister";
+import UserRegister from "./components/UserConnect";
 
 const routes =  [
     {
